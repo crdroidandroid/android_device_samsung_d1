6f0d0fb614fad55b72f17ab7b9d67529cc45b215
Orel <Orely4242@gmail.com>
2022-07-15 19:45:47 +0300

    d1: Adapt to crDroid

--- lineage_d1.mk
+++ lineage_d1.mk
@@ -1,48 +1,37 @@
 #
 # Copyright (C) 2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
 ## Inherit from generic products, most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-## Product API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
-
-## Inscreen Fingerprint HAL
-TARGET_HAVE_FOD := true
-
-## Inherit from d1 device
-$(call inherit-product, device/samsung/d1/device.mk)
-
-## Boot Animation
-TARGET_BOOTANIMATION_HALF_RES := true
-TARGET_SCREEN_HEIGHT := 2280
-TARGET_SCREEN_WIDTH := 1080
-
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+## Inherit from d1 device
+$(call inherit-product, device/samsung/d1/device.mk)
+
+## Inherit some common build stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+## Flags
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_HEIGHT := 2280
+TARGET_SCREEN_WIDTH := 1080
+TARGET_HAVE_FOD := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+TARGET_USES_AOSP_RECOVERY := true
+
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_d1
+PRODUCT_NAME := aosp_d1
 PRODUCT_DEVICE := d1
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-N970F
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := Galaxy Note10
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
