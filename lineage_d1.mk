62b84b8f9a19ee9cdfa524e4d9dfaa04591e082a
pedro <pxd.bin@gmail.com>
2025-01-30 08:44:07 -0300

    d1: crDroid bring up
    update overlay pixel pitch

--- lineage_d1.mk
+++ lineage_d1.mk
@@ -37,25 +37,21 @@ TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
 
 ## Inherit some common aosp stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Project-Elixir
+# CrDroid Stuff
 IS_PHONE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BUILD_APERTURE_CAMERA := true
-FORCE_LAWNCHAIR := true
-ELIXIR_BUILD_TYPE := OFFICIAL
-BUILD_USERNAME := FrEeRuNnEr4EvEr
-BUILD_HOSTNAME := Elixir
 TARGET_INCLUDE_STOCK_ACORE := false
 EXTRA_UDFPS_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1440
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := aosp_d1
+PRODUCT_NAME := lineage_d1
 PRODUCT_DEVICE := d1
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N970F
